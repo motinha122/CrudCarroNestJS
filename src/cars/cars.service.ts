505dd8170e7ba06a49a34af7d940a2aeb156b950
motinha122 <timota132@gmail.com>
2024-05-04 22:33:28 -0300

    :recycle: Update

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -32,17 +32,104 @@ export class CarsService {
   }
 
   async update(id: number, updateCarDto: UpdateCarDto): Promise<Car> {
+    await this.updatePlate(id, updateCarDto);
+    await this.updateModel(id, updateCarDto);
+    await this.updateColor(id, updateCarDto);
     await this.updatePrice(id, updateCarDto);
+    await this.updateDescription(id, updateCarDto);
+    await this.updateManufacture(id, updateCarDto);
+    await this.updateStatus(id, updateCarDto);
     const car = await this.findOne(id);
     console.log('Atualizando o carro id:' + id);
     return car;
   }
 
-  async updatePrice(id: number, updateCarDto: UpdateCarDto): Promise<void> {
+  private async updatePlate(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.plate) {
+      await this.carRepository.query(
+        'UPDATE car SET plate = $1 WHERE car.id = $2',
+        [updateCarDto.plate, id],
+      );
+    }
+    return;
+  }
+
+  private async updateModel(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.model) {
+      await this.carRepository.query(
+        'UPDATE car SET model = $1 WHERE car.id = $2',
+        [updateCarDto.model, id],
+      );
+    }
+    return;
+  }
+
+  private async updateColor(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.color) {
+      await this.carRepository.query(
+        'UPDATE car SET color = $1 WHERE car.id = $2',
+        [updateCarDto.color, id],
+      );
+    }
+    return;
+  }
+
+  private async updatePrice(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
     if (updateCarDto.price) {
       await this.carRepository.query(
         'UPDATE car SET price = $1 WHERE car.id = $2',
         [updateCarDto.price, id],
+      );
+    }
+    return;
+  }
+
+  private async updateDescription(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.description) {
+      await this.carRepository.query(
+        'UPDATE car SET description = $1 WHERE car.id = $2',
+        [updateCarDto.description, id],
+      );
+    }
+    return;
+  }
+
+  private async updateManufacture(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.manufacture) {
+      await this.carRepository.query(
+        'UPDATE car SET manufacture = $1 WHERE car.id = $2',
+        [updateCarDto.manufacture, id],
+      );
+    }
+    return;
+  }
+
+  private async updateStatus(
+    id: number,
+    updateCarDto: UpdateCarDto,
+  ): Promise<void> {
+    if (updateCarDto.status) {
+      await this.carRepository.query(
+        'UPDATE car SET status = $1 WHERE car.id = $2',
+        [updateCarDto.status, id],
       );
     }
     return;
